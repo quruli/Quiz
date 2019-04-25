2dee03a899ab8c3d9cd6fa5136befbaf3c9a49de
Pimblokto <quuruli@gmail.com>
2019-04-25 22:38:55 +0800

    -added quiz status -added different options accdg to user types

---
+++ perform.php
@@ -0,0 +1,10 @@
+<?php
+	require 'header.php';
+ ?>
+
+<body>
+	<div class="main-wrapper">
+		<h1 class="title">
+		</h1>
+	</div>
+</body>
--- quizdetails.php
+++ quizdetails.php
@@ -13,32 +13,58 @@
   </head>
   <body>
     <div class="main-wrapper">
-      <h2><?php echo $code; ?></h2>
+      <h2 class="title">
+        <a href="quiz.php">Quiz</a> >
+        <?php echo $code; ?>
+      </h2>
 
       <table>
         <tr>
           <th>Title</th>
-          <th>Course</th>
-          <th>Course Code</th>
           <th>Date Created</th>
+          <th>Status</th>
+          <th>Date Open</th>
+          <th>Date Closed</th>
           <th>Actions</th>
         </tr>
 
       <?php
+        $type = $_SESSION['type'];
         $sql = "SELECT * FROM quiz.list_of_quizzes WHERE course_code='$code' AND user_name='$user'; ";
         $result = mysqli_query($conn, $sql);
         $resultCheck = mysqli_num_rows($result);
+        $today = date('d-M-Y h:i A');
+        $status = "";
 
         if ($resultCheck > 0) {
           while ($row = mysqli_fetch_assoc($result)) {
+            $closeDate = date('d-M-Y h:i A', strtotime($row['date_closed']));
+            if ($today < $closeDate) {
+              $status = "Open";
+            } else {
+              $status = "Closed";
+            }
+
+
             echo '
               <tr>
                 <td>' . $row['quiz_title'] .'</td>
-                <td>' . $row['course_title'] . '</td>
-                <td>' . $row['course_code'] . '</td>
-                <td>' . date('d-M-Y', strtotime($row['date_created'])) .
-                '<td><button class="generic"><a href="quiz_edit.php?quiz_id=' . $row['quiz_id'] . '">Modify</a></button></td>
-              </tr>';
+                <td>' . date('d-M-Y', strtotime($row['date_created'])) . '</td>
+                <td>' . $status . '</td>
+                <td>' . date('d-M-Y h:i A', strtotime($row['date_open'])) . '</td>
+                <td>' . date('d-M-Y h:i A', strtotime($row['date_closed'])) . '</td>';
+
+                //date('d-M-Y') == DD-MMM-YYYY
+                //date('d-M-Y h:i A') == DD-MMM-YYYY HH:MM AA
+
+            echo '<td><button class="generic"><a href="';
+            if ($type == 2) { //teacher user type returns quiz_edit.php
+              echo 'quiz_edit.php?quiz_id=' . $row['quiz_id']. '">Modify</a></button></td>';
+            } else if ($type == 3) { //student returns perform.php
+              echo 'perform.php?quiz_id=' . $row['quiz_id'] . '">View</a></button></td>';
+            }
+
+            echo '</tr>';
           }
         } else if (empty($resultCheck)){
           echo 'No results found.<br />';
